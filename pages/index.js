d2bbf54f34a7b05817ad7e620645a8c57da90da5
kenjimiura89 <110425354+kenjimiura89@users.noreply.github.com>
2025-03-18 23:26:48 +0000

    Modifiquei a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home () { 
-    return <h1>  !Kenjinho testinho! </h1>,
-    <h2>oh yeah baby!</h2>
+    return 
+    <h1>  !Kenjinho testinho! </h1>,
+    <h2>oh yeah baby!</h2>,
+    <h2>Kenjinho, programinho! 😤</h2>
 }
 
 export default Home;
