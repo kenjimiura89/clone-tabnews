6102c14bfa6d627ff481ca7bed050caf4f2c85d2
kenjimiura89 <110425354+kenjimiura89@users.noreply.github.com>
2025-03-18 23:36:37 +0000

    Mudei a pagina

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-function Home () { 
-    return 
-    <h1>Kenjinho, programinho! 😤</h1>
+function Home() {
+    return (
+        <div>
+            <h1>Kenjinho Programinho! 😤</h1>
+            <p>Kenjinho ama Carolzinha</p>
+            <img src="https://pbs.twimg.com/profile_banners/1068620202416529410/1721492576/1500x500" alt="Imagem de Kenjinho e Carolzinha" />
+        </div>
+    );
 }
 
 export default Home;
